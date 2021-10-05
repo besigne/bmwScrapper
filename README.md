8c126f6199e46b0808c39d8638cd721c1fd83770
besigne <luciferxarcangel@gmail.com>
2021-10-04 23:36:56 -0300

    bmw scrapper, not compatible with multiple prices yet.

---
+++ README.md
@@ -0,0 +1,33 @@
+#Python Scrapper
+this is a basic scrapper that gets car models and his
+respective prices, should encounter some problems as 
+this is a first try, for example, multiple prices is
+something I hope be working for next update
+## 🚀 How to Use
+### 💻 Installing Bmw Scrapper
+Clone the repository to your local then proced to navigate
+through the terminal until you're inside /bmwscrapper, next
+thing you should do is run the following command:
+
+*Linux:*
+```
+python menu.py
+```
+***
+the navigation you should use the keyboard arrows and
+use enter to select the scrapper you want to use.
+As you select one scrapper it will notifcates if it worked
+properly and generates a .csv file on the current directory.
+***
+###Upcoming updates
+* BMW Scrapper
+- [x] BMW Models
+- [ ] Multiple Prices
+- [ ] Multiple Fuel types
+* Audi Scrapper
+- [ ] Audi Models
+- [ ] Multiple Prices
+- [ ] Multiple Fuel types
+****
+
+📝 Matheus Boesing da Silva
---
+++ scrapper/bmw.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from model.car import Car
+import csv
+
+page = requests.get('https://www.bmw.com.br/pt/all-models.html')
+
+soup = BeautifulSoup(page.text, "html.parser")
+car_models = soup.find_all('div', {'class': 'cmp-modelcard__entry'})
+
+BMW = []
+bmw = Car
+
+for cars in car_models:
+    car = cars.find('h5', {'class': 'cmp-modelcard__name'})
+    car = car.get_text()
+
+    fuel = cars.find('div', {'class': 'cmp-modelcard__fuel-type'})
+    fuel = fuel.get_text()
+    fuel = fuel.replace(' ', '')
+
+    price = cars.find('span', {'class': 'cmp-modelcard__price'})
+    price = price.get_text()
+    price = price.replace(' A partir de ', '')
+    price = price.replace(' à vista*. ', '')
+    price = price.replace(' à vista* ', '')
+    if price == '':
+        price = 'Multiple values, please verify website'
+
+    if fuel == '':
+        fuel = 'More than one version available, please verify website'
+
+    bmw = Car(name=car, price=price, fuel=fuel)
+    BMW = BMW + [bmw]
+
+filename = 'bmw.csv'
+
+try:
+    with open(filename, 'w', encoding='UTF-8', newline='') as f:
+        writer = csv.writer(f)
+        for row in BMW:
+            writer.writerow([row.name, row.price, row.fuel])
+
+except BaseException as e:
+    print('BaseException', filename)
+else:
+    print("Created CSV Successfully!")
